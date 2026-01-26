ec1925b604ce8b6e78dc312262a6f1959f484a4d
Nancy <mandubong1206@gmail.com>
2026-01-26 22:52:52 +0900

    BOJ1038 : 감소하는 수 (G5)

--- recursion/boj1038.cpp
+++ recursion/boj1038.cpp
@@ -3,7 +3,8 @@ BOJ1038 : 감소하는 수 (G5)
 
 해결 방법 : 
 시도 1 : 개수를 찾고, 숫자를 찾는 방식으로 하려고 했는데, 구현은 됐으나, 원하는 숫자가 안 나옴
-시도 2 : 숫자를 미리 만들고, sort해서 인덱스로 찾는 방식으로 진행
+시도 2 : 숫자를 미리 만들고, sort해서 인덱스로 찾는 방식으로 진행 -> int가 부족해서 틀렸습니다
+시도 3 : int 대신 long long으로 작성
 */
 
 #include <iostream>
@@ -12,13 +13,13 @@ BOJ1038 : 감소하는 수 (G5)
 
 using namespace std;
 
-vector<int> arr;
+vector<long long> arr;
 
-void find_num(int num) {
+void find_num(long long num) {
     arr.push_back(num);
     int last_digit = num % 10;
     for (int i = 0; i < last_digit; i++) {
-        int next_num = num * 10 + i;
+        long long next_num = num * 10 + i;
         find_num(next_num);
     }
 }
