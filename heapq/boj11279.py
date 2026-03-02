6ed017b54b376686f9d801145451133021aaa3bd
Nancy <mandubong1206@gmail.com>
2026-03-02 19:43:32 +0900

    BOJ11279 : 최대 힙 (S2)

---
+++ heapq/boj11279.py
@@ -0,0 +1,21 @@
+'''
+BOJ11279 : 최대 힙 (S2)
+
+해결 방법 :
+heapq.nlargest()를 활용하여 해결
+'''
+import sys, heapq
+input = sys.stdin.readline
+
+n = int(input())
+heap = []
+
+for _ in range(n):
+    i = int(input())
+    if i == 0:
+        if not heap:
+            print(0)
+            continue
+        print(-heapq.heappop(heap))
+    else:
+        heapq.heappush(heap, -i)
